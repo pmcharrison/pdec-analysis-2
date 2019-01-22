271e0e46d5a9556959884342ff0b0b8992d2d8f8
Peter M C Harrison <pmc.harrison@gmail.com>
2019-01-22 15:53:36 +0000

    Externalising PPM specifications

--- src/1-analyse/functions/get-ppm-spec.R
+++ src/1-analyse/functions/get-ppm-spec.R
@@ -1,29 +1,33 @@
 get_ppm_spec <- function() {
-  tribble(
-    ~ label,                     ~ buffer_len,   ~ stm_half_life, ~ stm_rate, ~ ltm_rate, ~ noise,
-    "Instant buffer decay",      18,            0.0000001,          1,          0.3,        0.5,
-    "Gradual buffer decay (I)",  15,            0.1,                1,          0.3,        0.5,
-    "Gradual buffer decay (II)", 15,            0.1,                1,          0.35,        0.5,
-    "Gradual buffer decay (III)",15,            0.08,               1,          0.35,        0.5,
-    "Gradual buffer decay (IV)", 15,            0.04,               1,          0.35,        0.5,
-    "Gradual buffer decay (V)",  18,            0.04,               1,          0.35,        0.5,
-    "Gradual buffer decay (VI)", 18,            0.04,               1,          0.5,        0.5,
-    "Gradual buffer decay (7)",  17,            0.04,               1,          0.42,        0.5,
-    "Gradual buffer decay (8)",  16,            0.04,               1,          0.42,        0.5,
-    "Gradual buffer decay (9)",  15,            0.04,               1,          0.42,        0.5,
-    "Gradual buffer decay (10)", 14,            0.04,               1,          0.42,        0.5,
-    # "Gradual buffer decay (VI)", 18,            0.125,              1,          0.35,        0.5
-    # "PPM1: Original",        Inf,            Inf,             1,          0,          0,
-    # "PPM2: + Noise",         Inf,            Inf,             1,          0,          0.75,
-    # "PPM3: + Memory decay",  0,            0.15,               1,          0,        0.75,
-    # "X",                     15,            0.001,             1,          0.7,        1,
-    # "stm_rate",          Inf,            Inf,           0.1,          0,        0,
-    # "stm_rate_noise",    Inf,            Inf,           0.1,          0,        1,
-    # "decay",             0,              0.5,           1,            0,        1,
-    # "decay_plus_buffer", 15,             0.5,           1,            0,        1,
-  ) %>% mutate(i = seq_len(n())) %>% 
+  read_csv("input/ppm-spec.csv", col_types = "cols")
+  %>% mutate(i = seq_len(n())) %>% 
     select(i, everything())
 }
+
+# "Original PPM",              Inf,            Inf,             1,          0,          0,
+# "Instant buffer decay",      18,            0.0000001,          1,        0.3,        0.5,
+# "Gradual buffer decay (V)",  18,            0.04,               1,        0.35,       0.5,
+# "Gradual buffer decay (VI)", 18,            0.04,               1,        0.5,        0.5
+# "Gradual buffer decay (I)",  15,            0.1,                1,         0.3,        0.5,
+# "Gradual buffer decay (II)", 15,            0.1,                1,          0.35,        0.5,
+# "Gradual buffer decay (III)",15,            0.08,               1,          0.35,        0.5,
+# "Gradual buffer decay (IV)", 15,            0.04,               1,          0.35,        0.5,
+# "Gradual buffer decay (7)",  17,            0.04,               1,          0.42,        0.5,
+# "Gradual buffer decay (8)",  16,            0.04,               1,          0.42,        0.5,
+# "Gradual buffer decay (9)",  15,            0.04,               1,          0.42,        0.5,
+# "Gradual buffer decay (10)", 14,            0.04,               1,          0.42,        0.5,
+# "Gradual buffer decay (11)", 18,            0.04,               1,          0.45,        0.5,
+# "Gradual buffer decay (12)", 18,            0.04,               1,          0.4,        0.5,
+# "Gradual buffer decay (13)", 18,            0.04,               1,          0.35,        0.5,
+# "Gradual buffer decay (VI)", 18,            0.125,              1,          0.35,        0.5
+# "PPM1: Original",        Inf,            Inf,             1,          0,          0,
+# "PPM2: + Noise",         Inf,            Inf,             1,          0,          0.75,
+# "PPM3: + Memory decay",  0,            0.15,               1,          0,        0.75,
+# "X",                     15,            0.001,             1,          0.7,        1,
+# "stm_rate",          Inf,            Inf,           0.1,          0,        0,
+# "stm_rate_noise",    Inf,            Inf,           0.1,          0,        1,
+# "decay",             0,              0.5,           1,            0,        1,
+# "decay_plus_buffer", 15,             0.5,           1,            0,        1,
 
 ppm_options_from_ppm_spec <- function(x) {
   if (is.data.frame(x)) stopifnot(nrow(x) == 1L)

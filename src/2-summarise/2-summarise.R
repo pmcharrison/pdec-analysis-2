e0fa64b3b512b6fb39dc2fa4ee56490f885af942
Peter M C Harrison <pmc.harrison@gmail.com>
2019-01-28 19:16:51 +0000

    Removing timestamped PDF

--- src/2-summarise/2-summarise.R
+++ src/2-summarise/2-summarise.R
@@ -28,9 +28,6 @@ p3 <- plot_example_trial(dat_analysis)
 
 P1 <- cowplot::plot_grid(p1, p2, ncol = 1, rel_heights = c(1.2, 2), labels = "AUTO")
 ggsave(plot = P1, filename = "output/by-cond.eps", width = 7.5, height = 7.5)
-ggsave(plot = P1, 
-       filename = glue::glue("output/by-cond-{Sys.time()}.pdf"),
-       width = 7.5, height = 7.5)
 ggsave(plot = p3, filename = "output/example-trial.eps", width = 5.5, height = 5.5)
 
 summary_subj %>% write_csv("output/summary-subj.csv")

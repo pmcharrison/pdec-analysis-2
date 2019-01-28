e00c729f1beb47085c8e638e3cbe1e9e753daa9c
Peter M C Harrison <pmc.harrison@gmail.com>
2019-01-28 19:17:08 +0000

    Augmenting inferential statistics

--- src/2-summarise/functions/do-stats-subj.R
+++ src/2-summarise/functions/do-stats-subj.R
@@ -1,3 +1,5 @@
+library(broom)
+
 do_stats_subj <- function(dat_response) {
   raw <- dat_response %>% 
     filter(cond == "randreg" & response == "hit") %>% 
@@ -9,6 +11,29 @@ do_stats_subj <- function(dat_response) {
     add_column(group = NA, .before = 1) %>% 
     mutate(group = seq_len(n()))
   
+  list(
+    wilcox = wilcox_subj(raw),
+    friedman = friedman_subj(raw)
+  )
+}
+
+friedman_subj <- function(raw) {
+  x <- raw$data %>% bind_rows() %>% split(., .$subj)
+  map(c(`10` = 10, `20` = 20), function(alph_size) {
+    map(x, function(subj_dat) {
+      c(25, 50, 75) %>% set_names(., .) %>% 
+        map(function(tone_len) {
+          subj_dat %>% 
+            filter(alphabet_size == alph_size & 
+                     tone_len_ms == tone_len) %>%
+            pull(rt_norm_tones_from_repeat)
+        }) %>% as_tibble()
+    }) %>% bind_rows() %>% as.matrix() %>% friedman.test() %>% glance() %>% 
+      add_column(alphabet_size = alph_size, .before = 1)
+  }) %>% bind_rows() %T>% write_csv("output/stats-subj-friedman.csv")
+}
+
+wilcox_subj <- function(raw) {
   gtools::combinations(n = nrow(raw), r = 2, v = raw$group) %>% 
     as.data.frame() %>% 
     set_names(c("group_1", "group_2")) %>% 
@@ -32,5 +57,5 @@ do_stats_subj <- function(dat_response) {
           by.x = "group_2", by.y = "group",
           suffixes = c("_1", "_2")) %>% 
     as_tibble() %T>% 
-    write_csv("output/stats-subj.csv")
+    write_csv("output/stats-subj-wilcox.csv")
 }
